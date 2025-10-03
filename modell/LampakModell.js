3c995400a1b4a574ce773d6b84c5c19eabb40295
Bori888 <ilyes.bori8@gmail.com>
2025-10-03 10:58:59 +0200

    model stilus index kész

--- modell/LampakModell.js
+++ modell/LampakModell.js
@@ -1,13 +1,41 @@
-import LampaAllapot from "LampaModell.js";
 export default class LampakModell {
-    #LampakLista = [];
 
-    #szuloElem;
+    #lampakLista = [];
+    #meret = 3;
+
     constructor() {
-        this.#LampakLista = LampakLista;
-        this.#szuloElem = szuloElem;
-        
-    }
-    lampaListaFeltolt() { }
 
+        this.lampakListaFeltolt();
+
+    }
+    lampakListaFeltolt() {
+        this.#lampakLista = [];
+        for (let i = 0; i < this.#meret; i++) {
+            let sor = [];
+            for (let j = 0; j < this.#meret; j++) {
+                sor.push(true);
+            }
+            this.#lampakLista.push(sor);
+        }
+    }
+
+    getLampak() {
+        return this.#lampakLista;
+    }
+
+    kapcsol(i, j) {
+        const iranyok = [[0, 0], [1, 0], [-1, 0], [0, 1], [0, -1]];
+        iranyok.forEach(([x, y]) => {
+            let ni = i + x, nj = j + y;
+            if (ni >= 0 && ni < this.#meret && nj >= 0 && nj < this.#meret) {
+                this.#lampakLista[ni][nj].valt();
+            }
+        });
+    }
+
+    nyertE() {
+        return this.#lampakLista.every(sor =>
+            sor.every(lampa => lampa.getAllapot() === false)
+        );
+    }
 }
